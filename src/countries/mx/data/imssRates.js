6fe8601d3986ed0e7a479c601d8efe6c1a9a1a30
elpenultimo <65920405+elpenultimo@users.noreply.github.com>
2025-12-19 10:32:04 -0300

    Add Mexico net salary calculator

---
+++ src/countries/mx/data/imssRates.js
@@ -0,0 +1,12 @@
+export const UMA_2025 = 108.57;
+
+export const IMSS_WORKER_RATES_2026 = {
+  enfermedadesMaternidad: {
+    exceedingFactor: 3, // applies on excedente de 3 UMA
+    rate: 0.004,
+  },
+  invalidezVida: 0.00625,
+  cesantiaVejez: 0.01125,
+};
+
+export default IMSS_WORKER_RATES_2026;
---
+++ src/countries/mx/data/isrTable.js
@@ -0,0 +1,15 @@
+export const ISR_MONTHLY_TABLE_2026 = [
+  { lowerLimit: 0.01, upperLimit: 746.04, fixedFee: 0, rate: 0.0192 },
+  { lowerLimit: 746.05, upperLimit: 6332.05, fixedFee: 14.32, rate: 0.064 },
+  { lowerLimit: 6332.06, upperLimit: 11128.01, fixedFee: 371.83, rate: 0.1088 },
+  { lowerLimit: 11128.02, upperLimit: 12935.82, fixedFee: 893.63, rate: 0.16 },
+  { lowerLimit: 12935.83, upperLimit: 15487.71, fixedFee: 1182.88, rate: 0.1792 },
+  { lowerLimit: 15487.72, upperLimit: 31236.49, fixedFee: 1640.18, rate: 0.2136 },
+  { lowerLimit: 31236.5, upperLimit: 49233, fixedFee: 5004.12, rate: 0.2352 },
+  { lowerLimit: 49233.01, upperLimit: 93993.9, fixedFee: 9236.89, rate: 0.3 },
+  { lowerLimit: 93993.91, upperLimit: 125325.2, fixedFee: 22665.17, rate: 0.32 },
+  { lowerLimit: 125325.21, upperLimit: 375975.62, fixedFee: 32691.18, rate: 0.34 },
+  { lowerLimit: 375975.63, upperLimit: Infinity, fixedFee: 117912.32, rate: 0.35 },
+];
+
+export default ISR_MONTHLY_TABLE_2026;
