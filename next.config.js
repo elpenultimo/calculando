669b6bc96a0a2fa688868d0a5e2341ac937a4c21
elpenultimo <65920405+elpenultimo@users.noreply.github.com>
2025-12-30 22:23:06 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,32 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      // TuCalculo
-      {
-        source: "/sitemap.xml",
-        has: [{ type: "host", value: "tucalculo.com" }],
-        destination: "/sitemap-tucalculo.xml",
-      },
-      {
-        source: "/sitemap.xml",
-        has: [{ type: "host", value: "www.tucalculo.com" }],
-        destination: "/sitemap-tucalculo.xml",
-      },
-
-      // Calculando
-      {
-        source: "/sitemap.xml",
-        has: [{ type: "host", value: "calculando.cl" }],
-        destination: "/sitemap-calculando.xml",
-      },
-      {
-        source: "/sitemap.xml",
-        has: [{ type: "host", value: "www.calculando.cl" }],
-        destination: "/sitemap-calculando.xml",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
